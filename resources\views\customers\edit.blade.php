a1f4b6b6fda855864f6189e90b5e4d24f9714aed
msu-bit-budsaparuch-63010914613 <119407200+msu-bit-budsaparuch-63010914613@users.noreply.github.com>
2023-09-24 15:36:41 +0700

    Create resources\views\customers\edit.blade.php

---
+++ resources\views\customers\edit.blade.php
@@ -0,0 +1,58 @@
+@extends('customers.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Customer</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('customers.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('customers.update',$customer->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="cus_name" value="{{ $customer->name }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="row">
+               <div class="col-xs-12 col-sm-12 col-md-12">
+                   <div class="form-group">
+                       <strong>Tel:</strong>
+                       <input type="number" name="cus_tel" value="{{ $customer->cus_tel }}"
+                       class="form-control" placeholder="Tel">
+                   </div>
+               </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Address:</strong>
+                    <textarea class="form-control" style="height:150px" name="cus_add"
+                     placeholder="Address">{{ $customer->cus_add }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
